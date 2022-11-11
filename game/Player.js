e88331d7b03929c541059d209ab9d70f1479e8be
Travis <travis.ralph@griffithuni.edu.au>
2022-11-12 01:17:57 +1000

    All animations added to Player.js during setup

--- game/Player.js
+++ game/Player.js
@@ -10,6 +10,8 @@ class Player{
         }
         this.w=30;
         this.h=100;
+
+        this.state = 0
     }
     controls(){
         if(keyIsDown(KEYS.A)){
@@ -28,6 +30,10 @@ class Player{
         if (keyIsDown(KEYS.S)) {
             this.goDown()
         }
+
+        if (keyIsDown(17)) {
+            this.crouch()
+        }
     }
     goLeft(){
         this.sprite.velocity.x -= playerSpeed;
@@ -41,8 +47,41 @@ class Player{
     goDown(){
         this.sprite.velocity.y += playerSpeed;
     }
+
+    crouch(){
+        this.sprite.changeAnimation("Player Crouch");
+        
+    }
     setup(){
         this.sprite=this.makePaddle(this.start.x,this.start.y,this.w, this.h)
+        //Movement
+        this.sprite.addAnimation("Player Walk", playerWalk)
+        this.sprite.addImage("Player Idle", playerIdle)
+        this.sprite.addAnimation("Player Crouch", playerCrouch)
+        this.sprite.addAnimation("Player Jump", playerJump)
+        this.sprite.addImage("Player Stand", playerStand)
+
+        //Combat
+        this.sprite.addImage("Player Guard", playerGuard)
+        this.sprite.addImage("Player GuardCrouch", playerGuardCrouch)
+        this.sprite.addImage("Player GuardJump", playerGuardJump)
+        this.sprite.addAnimation("Player Jab", playerJab)
+        this.sprite.addAnimation("Player JabCrouch", playerJabCrouch)
+        this.sprite.addAnimation("Player JabJump", playerJabJump)
+        this.sprite.addAnimation("Player HrzStrike", playerHrzStrike)
+        this.sprite.addAnimation("Player HrzStrikeCrouch", playerHrzStrikeCrouch)
+        this.sprite.addAnimation("Player HrzStrikeJump", playerHrzStrikeJump)
+        this.sprite.addAnimation("Player VertStrike", playerVertStrike)
+        this.sprite.addAnimation("Player VertStrikeCrouch", playerVertStrikeCrouch)
+        this.sprite.addAnimation("Player VertStrikeJump", playerVertStrikeJump)
+
+        //Player Damage
+        this.sprite.addAnimation("Player DmgAir", playerDmgAir)
+        this.sprite.addAnimation("Player DmgCrouch", playerDmgCrouch)
+        this.sprite.addAnimation("Player DmgHi", playerDmgHi)
+        this.sprite.addAnimation("Player DmgLo", playerDmgLo)
+        this.sprite.addAnimation("Player KO", playerKO)
+
     }
     draw(){
         this.sprite.collide(leftBorder, scrollLeft);
