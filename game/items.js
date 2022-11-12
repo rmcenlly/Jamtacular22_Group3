d59fcfbc063f83ddc2749c673bfb709959581c02
Tara Price <jasmosef@protonmail.com>
2022-11-12 19:15:50 +1000

    working on item collection

---
+++ game/items.js
@@ -0,0 +1,66 @@
+
+// attempting to create a generic item class 
+let beerImg;
+let beerSound;
+
+function preload () {
+beerImg = loadImage('images/beer_bottle.png');
+
+}
+
+class item{
+    constructor(){
+    this.pos.x = random (0, 500);
+    this.pos.y = random (200, 500);
+    this.itemSprite;
+    this.itemSound;
+    this.itemCollision;
+    }
+
+    addSprite(x, y, size){
+        this.itemSprite = createSprite(x, y, size, size);
+        this.itemSprite.setCollider('circle', 0, 0, size, size);
+    }
+
+    display(){
+        addSprite(this.pos.x, this.pos.y);
+    } 
+
+    addCollision(){
+        this.itemSprite.collide()
+    }
+    itemRemove(){
+        
+        this.itemSprite.remove();
+    }
+}
+
+
+
+class beer extends item{
+    constructor(){
+      super() //this calls the constructor in item
+    }
+    addSprite(x, y, size){
+      this.itemSprite=createSprite;
+      this.itemSprite.setCollider("circle", 0, 0, size, size);
+      this.itemSprite.addImage(beerImg);
+    }
+  }
+
+//dog.collide(grass2Group, gameOver);
+// cat.bounce(grass2Group, gameOver);
+// }
+
+// function dogDie(nutGroup, dog) {
+// dogHit.play();
+// dog.changeAnimation('die');
+// dog.life = 30;
+// nutGroup.remove();
+// }
+
+
+//   attachSounds(sound1, sound2);{
+//     this.sound1=sound1
+//   }
+ 
